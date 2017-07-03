0cd4851b681a14e411c5f226b2c7eb3ddd890a8b
gabietaylor <gabie.taylor@yahoo.com>
2017-07-02 21:08:56 -0400

    app using express logging and pathing to publuc

--- server.js
+++ server.js
@@ -5,3 +5,9 @@ var bodyParser = require('body-parser');
 // console.log(bodyParser);
 var methodOverride = require('method-override');
 // console.log(methodOverride);
+
+var app = express();
+// console.log(app);
+// applies content from pudlib dir.
+app.use(express.static(process.cwd() + '/public'));
+app.use(express.static('public'));
