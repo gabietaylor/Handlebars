33132350b945174f49a8f28e3d29c29b339f8963
gabietaylor <gabie.taylor@yahoo.com>
2017-07-03 07:47:05 -0400

    db table created

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,11 @@ CREATE DATABASE miscDB;
 
 -- USE DB
 USE miscDB;
+
+-- Create Table
+CREATE TABLE miscTable (
+    id INT NOT NULL AUTO_INCREMENT,
+    miscName VARCHAR(50),
+    miscBool BOOLEAN,
+    date TIMESTAMP
+);
