41ce49682f5a319f652d029275ddaedecc775cc2
gabietaylor <gabie.taylor@yahoo.com>
2017-07-02 21:06:55 -0400

    npms logging

---
+++ server.js
@@ -0,0 +1,7 @@
+// NPMs 
+var express = require('express');
+// console.log(express);
+var bodyParser = require('body-parser');
+// console.log(bodyParser);
+var methodOverride = require('method-override');
+// console.log(methodOverride);
