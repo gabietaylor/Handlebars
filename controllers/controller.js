44387e060e3a358994041fdd628af9069256360f
gabietaylor <gabie.taylor@yahoo.com>
2017-07-03 08:08:44 -0400

    redirecting routes

--- controllers/controller.js
+++ controllers/controller.js
@@ -10,29 +10,3 @@ var misc = require('../models/model.js');
 router.get('/', function(req, res) {
 	res.redirect('/index');
 });
-
-// index page renders all misc items to the DOM
-router.get('/index', function(req, res) {
-	misc.selectAll(function(data) {
-		var miscObject = { miscs: data };
-		console.log(miscObject);
-		res.render('index', miscObject);
-	});
-});
-
-// Create a new misc item
-router.post('/misc/create', function(req, res) {
-	misc.insertMisc(req.body.misc_name, function() {
-		res.redirect('/index');
-	});
-});
-
-// update a new misc item
-router.post('/misc/update/:id', function(req, res) {
-	misc.updateMisc(req.params.id, function() {
-		res.redirect('/index');
-	});
-});
-
-// exports routes
-module.exports = router;
