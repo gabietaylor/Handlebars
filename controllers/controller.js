7a6c1a2a4cfc63ad8e332daf631342e6bbbcb7a4
gabietaylor <gabie.taylor@yahoo.com>
2017-07-03 07:54:15 -0400

    npms logging

---
+++ controllers/controller.js
@@ -0,0 +1,7 @@
+// NPMs 
+var express = require('express');
+// console.log(express);
+var router = express.Router();
+// console.log(router);
+var model = require('../models/model.js');
+// console.log(model);
