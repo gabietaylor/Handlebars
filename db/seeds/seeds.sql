d293dfbc05c229b2c6cafa7cad20ab485280b409
gabietaylor <gabie.taylor@yahoo.com>
2017-07-03 13:40:27 -0400

    seeds inserted into db

---
+++ db/seeds/seeds.sql
@@ -0,0 +1,5 @@
+-- Seed Misc info in DB
+INSERT INTO miscTable (miscName, miscBool, date)
+VALUES ("miscFirstName", false, "2017-03-07 05:30:00"),
+  ("miscSecondName", true, "2017-03-07 05:30:01"),
+  ("miscThirdName", false, "2017-03-07 05:30:02");
